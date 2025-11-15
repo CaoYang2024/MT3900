077995153efb66f8b9c84f481cd3893cfcd33cff
yangcao <2233684404@qq.com>
2025-11-16 00:11:01 +0100

    11

--- src/kuksa/ultrasonic2kuksa.py
+++ src/kuksa/ultrasonic2kuksa.py
@@ -4,12 +4,15 @@ import threading
 
 
 class KuksaClient:
+    """
+    静默 publish，不阻塞 Ctrl+C，不打印任何内容
+    """
+
     def __init__(self, server="192.168.137.1:55555"):
         self.server = server
         self.image = "ghcr.io/eclipse-kuksa/kuksa-databroker-cli:main"
 
     def publish_async(self, signal, value):
-        """Non-blocking publish"""
         t = threading.Thread(
             target=self._publish_blocking,
             args=(signal, value),
@@ -18,10 +21,20 @@ class KuksaClient:
         t.start()
 
     def _publish_blocking(self, signal, value):
+        """完全静默执行 publish"""
+
         cmd = [
-            "docker", "run", "--rm", "--network=host",
+            "docker", "run",
+            "--rm",
             self.image,
             "--server", self.server,
+            "--protocol", "kuksa.val.v1",
             "publish", signal, str(value)
         ]
-        subprocess.run(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+        # 不要打印、不占用 terminal，不影响 Ctrl+C
+        subprocess.run(
+            cmd,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
